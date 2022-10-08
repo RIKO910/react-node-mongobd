8ed796c2127cc7f22aa783dd7c4d2a5a742e307e
RIKO910 <tarikulislamriko910@gmail.com>
2022-10-08 19:03:39 +0600

    lat commit

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,9 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Home = () => {
+    const [users,setUsers] =useState([]);
+    useEffect(()=>{
+        fetch('http://localhost:5000/user')
+        .then(res=>res.json())
+        .then(data=>setUsers(data))
+    },[])
+    const handleDelete = id=>{
+            const proceed =window.confirm('are you sure you want to delete?');
+            if(proceed){
+                console.log('deleting id',id)
+                const url =`http://localhost:5000/user/${id}`
+                fetch(url,{
+                    method:'delete'
+                })
+                .then(res=>res.json())
+                .then(data=>{ 
+                    console.log('deleted successful',data)
+                    if(data.deletedCount=0){
+                            console.log('deleted successful',data)
+                            const remain =users.filter(user=>user._id !==id);
+                            setUsers(remain)
+                    }
+                        
+                })
+                
+            }
+            
+    }
     return (
         <div>
             <h2>this is home</h2>
+            <ul>
+            {
+                users.map(user=><li key={user._id}>{user.name}::{user.email}
+                <button onClick={()=>handleDelete(user._id)} >delete</button>
+                </li>)
+            }
+            </ul>
+            
         </div>
     );
 };
